72c6fdda4c5099d7cf29a87181ede0475fa0878a
nikolenkoanton92 <nikolenkoanton92@gmail.com>
2015-01-05 15:16:43 -0800

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res) {
+  res.send('<h1>Hello World</h1>');
+});
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
