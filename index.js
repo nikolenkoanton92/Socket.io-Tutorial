6e981d057d4e9403e8214a8372ccdeeaff19c265
nikolenkoanton92 <nikolenkoanton92@gmail.com>
2015-01-05 15:43:45 -0800

    added emit for messager

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
+  socket.on('chat message', function(msg) {
+    console.log('message: ' + msg);
+  });
   console.log('a user connected');
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, function() {
